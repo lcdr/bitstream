b3389c8cabc025de12fb859597140223c4a01a25
lcdr <lcdr@users.noreply.github.com>
2020-04-17 16:27:36 +0200

    Migrate to GitHub

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	version="0.1.0",
 	description="Sequential bit-level reading and writing.",
 	author="lcdr",
-	url="https://bitbucket.org/lcdr/bitstream/",
+	url="https://github.com/lcdr/bitstream/",
 	license="GPL v3",
 	packages=["bitstream"],
 	python_requires=">=3.6",
